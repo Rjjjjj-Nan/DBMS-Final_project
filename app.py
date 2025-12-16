cf980aff88644f306acfef7dbf70dc0d4c8e6b62
Rjjjjj-Nan <johnraymitra1@gmail.com>
2025-12-17 01:21:19 +0800

    FIX

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def login_required(f):
 
 @app.route('/')
 def home():
-    return render_template('home.html', title = 'Lost Link')
+    return render_template('home.html', title = 'LOSTLINK')
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
